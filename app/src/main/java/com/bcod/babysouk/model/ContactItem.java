233111d17b0401fa5bce8920fb01ab88c2fb8599
vidyesh95 <vidyesh95@gmail.com>
2020-05-08 11:34:10 +0530

    22 commit

---
+++ app/src/main/java/com/bcod/babysouk/model/ContactItem.java
@@ -0,0 +1,13 @@
+package com.bcod.babysouk.model;
+
+public class ContactItem {
+    private int countryName;
+
+    public ContactItem(int countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCountryName() {
+        return countryName;
+    }
+}
